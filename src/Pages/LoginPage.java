385ca9d85755c0cccacbf349d9de0a68e5e86326
Swe-th <swethayn@outlook.com>
2025-03-17 22:40:08 -0700

    add/deleted/modified the few file

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -1,5 +1,12 @@
 package Pages;
 
 public class LoginPage {
+	public static void main(String a[]) {
+		System.out.println("Hello LP");
+		String un ="admin";
+		String pwd ="admin";
+		System.out.println("Logged in..");
+		
+	}
 
 }
