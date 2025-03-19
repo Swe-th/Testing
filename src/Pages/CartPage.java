8cde60cef908dd1eebf18176edc14510727b3003
Swe-th <swethayn@outlook.com>
2025-03-18 20:49:05 -0700

    updated Cart page

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -5,6 +5,7 @@ public class CartPage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Cart Page");
+		System.out.println("Cart Page Feature");
 	}
 
 }
